01a98c7667ca5ea2947c68bb8f9c3fe141e25a29
tboutefara <tarek_ini@yahoo.fr>
2022-05-22 21:27:07 +0100

    Another scene completed or almost, dem almost ready

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -1,5 +1,6 @@
 let game = new Game(name);
 let bubble;
+let cars = [];
 
 let categories = ["علوم الحاسب", "علوم الطبيعة", "حياة يومية", "تاريخ", "جغرافيا", "ثقافة", "العمل", "أسفار وسياحة", "رياضة"];
 let chosenCategory = "";
@@ -39,11 +40,16 @@ let questions = [
 let wordsxy = [];
 let buttons = [];
 let questionxy = [];
+let carxy = [];
 let currentQuestion = 0;
+let currentCar = 0;
 
 
 function preload(){
 	bubble = loadImage('../img/bubble.png');
+	for(var i = 1; i < 7; i++){
+		cars[i - 1] = loadImage('../img/car0' + i + '.png');
+	}
 }
 
 function setup() {
@@ -123,7 +129,7 @@ function prepareFirstScene(){
 	
 	for(var i = 0; i < 3; i++){
 		for(var j = 0; j < 3; j++){
-			wordsxy[3 * i + j] = {x : x + random(150) + j * 200, y : y + random(50)};
+			wordsxy[3 * i + j] = {"x" : x + random(150) + j * 200, "y" : y + random(50)};
 		}
 		x = 100;
 		y += 200;
@@ -155,6 +161,24 @@ function drawFirstScene(){
 }
 
 function prepareSecondScene(){
+	let x = 400;
+	let y = 250;
+	
+	questionxy[0] = {"x" : x, "y" : y};
+	y += 100;
+	for(var i = 0; i < 3; i++){
+		questionxy[i + 1] = {"x" : x, "y" : y};
+		y += 50;
+	}
+	
+	x = 25;
+	y = 470;
+	
+	for(var i = 0; i < 7; i++){
+		carxy[i] = {"x" : x, "y" : y};
+		x += 130;
+	}
+	
 	game.scene = 5;
 }
 
@@ -169,20 +193,41 @@ function drawSecondScene(){
 	textSize(48);
 	textAlign(CENTER);
 	text(chosenWord, 400, 175);	
+	
+	textSize(28);
+	textAlign(CENTER);
+	text(questions[currentQuestion].question, questionxy[0].x, questionxy[0].y);
+	
+	textSize(24);
+	textAlign(CENTER);
+	for(var i = 0; i < 3; i++){
+		text(questions[currentQuestion].choices[i], questionxy[i + 1].x, questionxy[i + 1].y);
+	}
+	
+	stroke(0, 255, 50);
+	strokeWeight(4);
+	rect(carxy[currentCar].x, carxy[currentCar].y + 10, 100, 60); // The 10 is to align, no math behind the rect to images
+	stroke(0);
+	strokeWeight(0);
+	
+	imageMode(CORNER);
+	for(var i = 0; i < 6; i++){
+		image(cars[i], carxy[i].x, carxy[i].y, 100, 60);
+	}
 }
 
 function mouseClicked(){
 	console.log("clicked " + mouseX + "  " + mouseY);
 	if(game.scene == 1){
-		choseACategory();
+		chooseACategory();
 	}else if(game.scene == 3){
-		choseAWord();
+		chooseAWord();
 	}else if(game.scene == 5){
 		answerAQuestion();
 	}
 }
 
-function choseACategory(){
+function chooseACategory(){
 	var choice = -1;
 	var choiceFound = false;
 	var i = 0;
@@ -202,7 +247,7 @@ function choseACategory(){
 	}
 }
 
-function choseAWord(){
+function chooseAWord(){
 	var choice = -1;
 	var choiceFound = false;
 	var i = 0;
@@ -219,5 +264,31 @@ function choseAWord(){
 	
 	if(choice != -1){
 		game.scene = 4;
+		chosenWord = words[choice];
+	}
+}
+
+function answerAQuestion(){
+	var choice = -1;
+	var choiceFound = false;
+	var i = 0;
+	
+	while(!choiceFound && i < 3){ // We always have 3 choices, if changed, this value must be updated
+		if(mouseX > questionxy[i + 1].x - 100 && mouseX < questionxy[i + 1].x + 100 // We suppose
+		&& mouseY > questionxy[i + 1].y - 40 && mouseY < questionxy[i + 1].y){
+			choice = i;
+			choiceFound = true;
+		}else{
+			i++;
+		}
+	}
+	
+	if(choice != -1){
+		if(choice == questions[currentQuestion].answer){
+			currentCar++;
+		}else{
+			currentCar = currentCar == 0 ? 0 : currentCar - 1;
+		}
+		currentQuestion++;
 	}
 }
